410bfff636d2de2f48ae66015a587d5b28327360
tridun <tristandunlop@gmail.com>
2021-01-24 19:24:47 -0800

    Lab 4 additions

---
+++ lab4/js/lab.js
@@ -0,0 +1,17 @@
+// Import random integer function
+
+// Make an array of strings containing "Rock", "Paper", and "Scissors"
+
+// Assign a random play to the computer from 0-2 using random integer picker function (rock = 0, paper = 1, scissors = 3)
+
+// Ask for user input
+  // Use conditionals to test for appropriate input
+    // If not one of the 3 options, print error
+
+// Test for win condition using if else statements
+  // If user_input beats computer_input
+    // Print "YOU WIN" - user_input + " beats " + computer_input + "!"
+  // If else computer_input beats user_input
+    // Print "YOU LOSE" - computer_input + " beats " + user_input + " :("
+  // Else
+    // Print "It's a tie!"
