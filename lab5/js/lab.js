3c93c5b51587c5939d55f13ff0bc3460d589e84c
tridun <tristandunlop@gmail.com>
2021-01-26 17:28:40 -0800

    lab 5 creation

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/*
+ * Authors: Tristan Dunlop and Ella Tracy <tdunlop@ucsc.edu> <eptracy@ucsc.edu>
+ * Created: January 26th, 2021
+ * License: Public Domain
+ */
+
+// declaration of variables
+var carMake = "Chevrolet";
+var carModel = "Corvette Stingray";
+var color = "monza red";
+var year = 1969;
+var ownIt = false;
+
+// age calculation
+var age = 2021 - year;
+
+// printed out values
+document.writeln("Make: " + carMake + "<br>");
+document.writeln("Model: " + carModel + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " yrs.<br>");
+document.writeln("In Possession: " + ownIt + "<br>");
